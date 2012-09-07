3903d3238001e9dbc0c248deb3c62ddfbfd63ec3
zipp <joshua.farrar@gmail.com>
2012-09-06 21:36:05 -0400

    Removed Express dependency

---
+++ index.js
@@ -0,0 +1,18 @@
+var http = require('http')
+  , fs = require('fs')
+  , send = require('send')
+  , ejs = require('ejs')
+  , port = process.env.port || 8080
+
+httpServer = http.createServer(function (req, res) {
+  console.log('%s - %s', req.method, req.url);
+  if (req.method === 'GET' && req.url ==='/') {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.end(ejs.render(fs.readFileSync(__dirname + '/views/index.ejs', 'utf8'), { title: 'Noob MMO' }));
+  }
+
+  send(req, req.url)
+  .root(__dirname + '/public')
+  .pipe(res)
+
+}).listen(port);
